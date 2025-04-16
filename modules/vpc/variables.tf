11a0b4be7892348ea8e9db68362ebcf21d0bb2b3
cloud-infraluxantiq <cloud-infra@luxantiq.com>
2025-04-16 12:24:40 +0530

    Update variables.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,11 +1,18 @@
+############################################################
+# Input Variables: Luxantiq VPC Module
+# Defines core VPC, subnet, NAT, and firewall configuration
+############################################################
 
+# Name of the custom VPC
 variable "vpc_name" {
   description = "Name of the VPC network"
   type        = string
 }
 
+# Map of named subnets
+# Each key is the subnet name and maps to CIDR, region, and optional secondary IP ranges
 variable "subnets" {
-  description = "Map of subnets with CIDR and region"
+  description = "Map of subnets with CIDR, region, and optional secondary ranges"
   type = map(object({
     cidr             = string
     region           = string
@@ -13,23 +20,27 @@ variable "subnets" {
   }))
 }
 
+# Region to deploy Cloud NAT and Cloud Router
 variable "nat_region" {
   description = "Region for NAT configuration"
   type        = string
 }
 
+# Region to create the VPC connector for Cloud Run / Serverless
 variable "vpc_connector_region" {
   description = "Region for VPC Serverless Connector"
   type        = string
 }
 
+# CIDR block used by the VPC connector (must not overlap with subnets)
 variable "vpc_connector_cidr" {
   description = "CIDR range for VPC connector"
   type        = string
 }
 
+# Map of firewall rule definitions keyed by rule name
 variable "firewall_rules" {
-  description = "Map of firewall rule configurations"
+  description = "Map of firewall rule configurations (allow ingress)"
   type = map(object({
     protocol      = string
     ports         = list(string)
