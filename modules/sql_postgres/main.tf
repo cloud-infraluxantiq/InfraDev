42a420eeace7c16ed69c715e5100c44e38d73041
cloud-infraluxantiq <cloud-infra@luxantiq.com>
2025-04-16 12:15:11 +0530

    Update main.tf

--- modules/sql_postgres/main.tf
+++ modules/sql_postgres/main.tf
@@ -1,4 +1,9 @@
+# ----------------------------------------------------------
+# Luxantiq SQL Postgres Module
+# Provisions private Cloud SQL instance with IAM + backups
+# ----------------------------------------------------------
 
+# Cloud SQL Instance (PostgreSQL 14) with private IP
 resource "google_sql_database_instance" "postgres_instance" {
   name             = var.instance_name
   region           = var.region
@@ -6,28 +11,31 @@ resource "google_sql_database_instance" "postgres_instance" {
   project          = var.project_id
 
   settings {
-    tier = var.tier
+    tier              = var.tier
     availability_type = "REGIONAL"
     disk_autoresize   = true
     disk_type         = "PD_SSD"
     disk_size         = var.disk_size
     activation_policy = "ALWAYS"
+
     backup_configuration {
-      enabled            = true
-      start_time         = "03:00"
+      enabled                        = true
+      start_time                     = "03:00"
       point_in_time_recovery_enabled = true
-      project = var.project_id
-}
+    }
+
     maintenance_window {
-      day          = 7
+      day          = 7  # Sunday
       hour         = 2
       update_track = "stable"
     }
+
     ip_configuration {
-      ipv4_enabled    = false
-      private_network = var.private_network
+      ipv4_enabled    = false                   # Enforce private IP only
+      private_network = var.private_network     # VPC network self-link
       require_ssl     = true
     }
+
     database_flags = var.database_flags
   }
 
@@ -35,25 +43,28 @@ resource "google_sql_database_instance" "postgres_instance" {
   encryption_key_name = var.encryption_key_name
 }
 
+# SQL Users (from var.users map)
 resource "google_sql_user" "users" {
   for_each = var.users
 
   name     = each.key
   instance = google_sql_database_instance.postgres_instance.name
   password = each.value.password
-  project = var.project_id
+  project  = var.project_id
 }
 
+# SQL Databases (from var.databases list)
 resource "google_sql_database" "databases" {
   for_each = toset(var.databases)
 
   name     = each.value
   instance = google_sql_database_instance.postgres_instance.name
-  project = var.project_id
+  project  = var.project_id
 }
 
+# IAM binding to allow a specific SA to administer Cloud SQL
 resource "google_project_iam_member" "cloudsql_admin" {
   role   = "roles/cloudsql.admin"
-  member = "serviceAccount:${var.service_account_email  project = var.project_id
-}"
+  member = "serviceAccount:${var.service_account_email}"
+  project = var.project_id
 }
