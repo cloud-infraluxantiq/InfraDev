1a49d5b6abcf06d98201290e33e311ea5d4375f7
cloud-infraluxantiq <cloud-infra@luxantiq.com>
2025-04-16 12:20:41 +0530

    Update outputs.tf

--- modules/storage/outputs.tf
+++ modules/storage/outputs.tf
@@ -1,8 +1,17 @@
 
+############################################################
+# Outputs: Luxantiq Storage Module
+# Exposes bucket names and URLs for reference in other modules
+############################################################
+
+# List of all bucket names created
 output "bucket_names" {
-  value = [for b in google_storage_bucket.buckets : b.name]
+  description = "Names of all provisioned GCS buckets"
+  value       = [for b in google_storage_bucket.buckets : b.name]
 }
 
+# List of bucket URLs (gs://)
 output "bucket_urls" {
-  value = [for b in google_storage_bucket.buckets : "gs://${b.name}"]
+  description = "GCS-formatted bucket URLs (gs://bucket-name)"
+  value       = [for b in google_storage_bucket.buckets : "gs://${b.name}"]
 }
