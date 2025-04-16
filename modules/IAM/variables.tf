809c5c3ca1c3241c1137ad0a32f6dfff5562f4ec
cloud-infraluxantiq <cloud-infra@luxantiq.com>
2025-04-16 09:57:32 +0530

    Update variables.tf

--- modules/IAM/variables.tf
+++ modules/IAM/variables.tf
@@ -1,14 +1,26 @@
+# terraform.tfvars
+# -----------------
+# Service account setup for Luxantiq dev provisioning and CI/CD
 
-variable "project_id" {
-  type = string
-}
+service_accounts = {
+  terraform-deployer = {
+    display_name = "Terraform Deployer"
+    description  = "Used by Terraform to manage GCP infrastructure"
+    role         = "roles/editor"
+    create_key   = true
+  }
 
-variable "service_accounts" {
-  description = "Map of service accounts and their role assignments"
-  type = map(object({
-    display_name = string
-    description  = string
-    role         = string
-    create_key   = bool
-  }))
+  jenkins-agent = {
+    display_name = "Jenkins Build Agent"
+    description  = "Used by Jenkins to trigger builds and manage deployments"
+    role         = "roles/cloudbuild.builds.editor"
+    create_key   = true
+  }
+
+  cloud-scheduler-executor = {
+    display_name = "Scheduler Executor"
+    description  = "Used by Cloud Scheduler to publish to Pub/Sub"
+    role         = "roles/pubsub.publisher"
+    create_key   = false
+  }
 }
