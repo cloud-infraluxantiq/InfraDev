5a784ba10b8c7cf19e9865657fba86de3358ecfa
cloud-infraluxantiq <cloud-infra@luxantiq.com>
2025-04-16 10:42:43 +0530

    Update outputs.tf

--- modules/dns_ssl/outputs.tf
+++ modules/dns_ssl/outputs.tf
@@ -1,8 +1,16 @@
+# DNS + SSL Outputs for Visibility and Cross-Module Use
 
 output "load_balancer_ip" {
-  value = google_compute_global_address.lb_ip.address
+  description = "Global static IP address for HTTPS Load Balancer"
+  value       = google_compute_global_address.lb_ip.address
 }
 
-output "ssl_status" {
-  value = google_compute_managed_ssl_certificate.ssl_cert.managed.status
+output "ssl_certificate_id" {
+  description = "Self-link of the managed SSL certificate"
+  value       = google_compute_managed_ssl_certificate.ssl_cert.id
+}
+
+output "forwarding_rule_url" {
+  description = "Global forwarding rule target URL"
+  value       = google_compute_global_forwarding_rule.https_forwarding.self_link
 }
