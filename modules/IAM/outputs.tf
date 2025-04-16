11bedc8e37ca84952692d3518aa1eee5f993b2ab
cloud-infraluxantiq <cloud-infra@luxantiq.com>
2025-04-16 10:01:32 +0530

    Update outputs.tf

--- modules/IAM/outputs.tf
+++ modules/IAM/outputs.tf
@@ -1,9 +1,24 @@
+##############################################
+# Outputs: IAM Service Accounts and Keys
+# This module exports useful references
+# for pipelines, audit logging, or external usage.
+##############################################
 
+# ------------------------------------------------
+# List of all created service account email IDs
+# Useful for reference in other modules or roles
+# ------------------------------------------------
 output "service_account_emails" {
-  value = [for sa in google_service_account.service_accounts : sa.email]
+  description = "List of all service account email addresses created by this module"
+  value       = [for sa in google_service_account.service_accounts : sa.email]
 }
 
+# ------------------------------------------------
+# (Optional) Exported private keys of service accounts
+# Only includes keys for accounts with `create_key = true`
+# ------------------------------------------------
 output "service_account_keys" {
+  description = "Map of service account keys (base64-encoded private keys)"
   value = {
     for k, v in google_service_account_key.keys :
     k => v.private_key
