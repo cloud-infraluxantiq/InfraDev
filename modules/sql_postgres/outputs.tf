45fafdff28139cd3f74b7081601166902ee38c29
cloud-infraluxantiq <cloud-infra@luxantiq.com>
2025-04-18 11:03:18 +0530

    Update outputs.tf

--- modules/sql_postgres/outputs.tf
+++ modules/sql_postgres/outputs.tf
@@ -5,19 +5,12 @@
 ############################################################
 
 # Cloud SQL instance name (used in Cloud Run / Secrets)
-output "instance_name" {
-  description = "Name of the Cloud SQL PostgreSQL instance"
-  value       = google_sql_database_instance.postgres_instance.name
-}
-
-# Full connection name: project:region:instance (used in Django Cloud Run deploy)
 output "connection_name" {
-  description = "Cloud SQL connection string for Django (used by Cloud Run)"
   value       = google_sql_database_instance.postgres_instance.connection_name
+  description = "Connection string used by Cloud Run to connect to Cloud SQL"
 }
 
-# Private IP of the instance (used in VPC-based DB access)
-output "private_ip_address" {
-  description = "Primary private IP address of the Cloud SQL instance"
-  value       = google_sql_database_instance.postgres_instance.ip_address[0].ip_address
+output "instance_name" {
+  value       = google_sql_database_instance.postgres_instance.name
+  description = "Name of the Cloud SQL instance"
 }
