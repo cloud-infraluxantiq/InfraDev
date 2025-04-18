64967bbcf233c471dffb839477be937238a754c2
cloud-infraluxantiq <cloud-infra@luxantiq.com>
2025-04-18 19:14:50 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -183,11 +183,33 @@ variable "timeout_seconds" {
   type        = number
   description = "Timeout for Cloud Run service"
 }
+# --------------------------
+# VPC
+# --------------------------
 variable "vpc_connector" {
   type        = string
   description = "Name of the VPC connector to attach"
 }
+variable "vpc_connector_cidr" {
+  description = "CIDR range for the VPC Serverless connector"
+  type        = string
+}
 
+variable "vpc_connector_region" {
+  type        = string
+  description = "Region to deploy VPC connector"
+}
+variable "vpc_name" {
+  description = "Name of the VPC network"
+  type        = string
+}
+variable "private_network" {
+  description = "The self-link of the VPC network to connect Cloud SQL"
+  type        = string
+}
+
+
+# --------------------------
 variable "memory_limit" {
   type        = string
   description = "Memory allocated for the container"
@@ -218,16 +240,6 @@ variable "django_secret_key_secret" {
 }
 variable "nat_region" {
   description = "Region used for Cloud NAT and Router"
-  type        = string
-}
-
-variable "vpc_name" {
-  description = "Name of the VPC network"
-  type        = string
-}
-
-variable "private_network" {
-  description = "The self-link of the VPC network to connect Cloud SQL"
   type        = string
 }
 variable "razorpay_api_key_secret" {
@@ -261,21 +273,9 @@ variable "users" {
     password = string
   }))
 }
-
-variable "vpc_connector_cidr" {
-  description = "CIDR range for the VPC Serverless connector"
-  type        = string
-}
-
-variable "vpc_connector_region" {
-  description = "Region where VPC connector will be created"
-  type        = string
-}
-
-variable "vpc_name" {
-  description = "Name of the custom VPC"
-  type        = string
-}
+# --------------------------
+# FIrewall
+# --------------------------
 
 variable "firewall_rules" {
   description = "Map of firewall rule configurations"
