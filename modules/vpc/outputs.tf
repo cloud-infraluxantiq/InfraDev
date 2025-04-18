fc0a2b946bb32d133e201a5934bc9699ba27a654
cloud-infraluxantiq <cloud-infra@luxantiq.com>
2025-04-18 19:32:58 +0530

    Update outputs.tf

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -8,10 +8,6 @@ output "vpc_name" {
   description = "The name of the custom VPC network"
   value       = google_compute_network.custom_vpc.name
 }
-output "vpc_connector_name" {
-  value = google_vpc_access_connector.vpc_connector.name
-}
-
 # List of all subnet names created in this VPC
 output "subnet_names" {
   description = "Names of subnets created within the VPC"
