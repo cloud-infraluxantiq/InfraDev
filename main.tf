90591021116f221d482ddb7f1cce4ca60eb77b65
cloud-infraluxantiq <cloud-infra@luxantiq.com>
2025-04-18 12:43:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,6 +27,19 @@ module "cloud_run_django" {
   source     = "./modules/cloud_run_django"
   project_id = var.project_id
   region     = var.region
+
+  service_name             = var.cloud_run_django_service_name
+  image_url                = var.django_image_url
+  timeout_seconds          = 300
+  database_connection_name = module.sql_postgres.connection_name
+  secret_env_vars = {
+    DB_PASSWORD  = var.db_password_secret
+    JWT_SECRET   = var.jwt_secret_secret
+    SECRET_KEY   = var.django_secret_key_secret
+  }
+  vpc_connector = module.vpc.vpc_connector_name
+  memory_limit  = "512Mi"
+  concurrency   = 80
 }
 
 # ----------------------------------
@@ -36,7 +49,17 @@ module "cloud_run_angular" {
   source     = "./modules/cloud_run_angular"
   project_id = var.project_id
   region     = var.region
+
+  service_name    = var.cloud_run_angular_service_name
+  image_url       = var.angular_image_url
+  iam_member      = "allUsers"
+  custom_domain   = var.angular_domain
+  concurrency     = 80
+  timeout_seconds = 300
+  memory_limit    = "512Mi"
+  max_scale       = "10"
 }
+
 
 # ----------------------------
 # Cloud SQL (PostgreSQL) Setup
