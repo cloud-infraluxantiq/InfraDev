6cb24eb688c91ad39a7f013b911335cc4769baa9
cloud-infraluxantiq <cloud-infra@luxantiq.com>
2025-04-18 11:06:35 +0530

    Update outputs.tf

--- modules/lb/outputs.tf
+++ modules/lb/outputs.tf
@@ -3,7 +3,7 @@
 # Used to link to DNS + SSL module
 ############################################################
 
-output "url_map_self_link" {
-  description = "Self-link of the URL map created for HTTP(S) Load Balancer"
-  value       = google_compute_url_map.default.self_link
+output "lb_ip_address" {
+  value       = google_compute_global_address.lb_ip.address
+  description = "The external IP address allocated for the HTTPS Load Balancer"
 }
