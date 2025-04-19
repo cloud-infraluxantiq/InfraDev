2ac6f65219a710b7f95aba6cbd1b59002688a2ae
cloud-infraluxantiq <cloud-infra@luxantiq.com>
2025-04-19 07:53:36 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -21,11 +21,27 @@ variable "project_number" {
   type        = string
   description = "The numeric project ID (used in IAM bindings, policies)"
 }
+
 variable "concurrency" {
   description = "Concurrency value for Cloud Run service"
   type        = number
   default     = 80  # or remove default if you want to pass via GitHub secrets
 }
+variable "cloud_sql_instance_name" {
+  type        = string
+  description = "Cloud SQL instance name"
+}
+
+variable "angular_image_url" {
+  type        = string
+  description = "Image URL for Angular frontend"
+}
+
+variable "django_domain" {
+  type        = string
+  description = "Custom domain for Django backend"
+}
+
 # --------------------------
 # IAM
 # --------------------------
