646447aa61362371cf0e4b08abdf345c475ac7d1
cloud-infraluxantiq <cloud-infra@luxantiq.com>
2025-04-19 20:57:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,11 +15,9 @@ data "google_secret_manager_secret_version" "service_account_email" {
   secret  = var.service_account_email_secret
   project = var.project_id
 }
-
 locals {
   service_account_email = data.google_secret_manager_secret_version.service_account_email.secret_data
 }
-
 
 # ------------------------
 # Google Provider Setup
