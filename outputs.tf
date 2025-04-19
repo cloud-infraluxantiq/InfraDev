ae9690f730ad7fcd7ca21f3a9fa278460b2e2150
cloud-infraluxantiq <cloud-infra@luxantiq.com>
2025-04-19 14:21:38 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -37,3 +37,7 @@ output "cloud_sql_connection_name" {
   value       = module.sql_postgres.connection_name
   description = "Cloud SQL connection string"
 }
+output "debug_env_vars_keys" {
+  description = "Keys present in env_vars (environment variable names for Cloud Run)"
+  value       = keys(var.env_vars)
+}
